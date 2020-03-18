217b66af62e4c66ec7b2c0de23574a41b25a31fb
robertito_9010@hotmail.com <Eduardo24>
2020-03-18 00:32:58 -0500

    PrimerSubida

---
+++ Microservice/ProductMicroservice/Persistance/ProductDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductMicroservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroservice.Persistance
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Product A",
+                    Description = "Product A"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Product B",
+                    Description = "Product B"
+                }
+           );
+        }
+    }
+}
---
+++ Microservice/ProductMicroservice/Persistance/Repositories/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductMicroservice.Models;
+
+namespace ProductMicroservice.Persistance.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductDbContext _context;
+        public ProductRepository(ProductDbContext context) => _context = context;
+
+        public void DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductId(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
